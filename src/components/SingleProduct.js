46fabafc0811f148ae95be120b3237a2d1ee1709
Andres Soca <lumen.development01@gmail.com>
2022-06-14 14:01:14 -0400

    working on the singleProducts feature to display product info and images

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,1 +1,26 @@
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
+const SingleProduct = ({ prod }) => {
+  return (
+    <div className="products">
+      <Card>
+        <Card.Img variant='top' src={ prod.image } alt={ prod.name }/>
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span> ${prod.price.split(".")[0]} </span>
+            {prod.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div>4 days delivery</div>
+            )}
+            <Rating rating={ prod.ratings } />
+          </Card.Subtitle>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default SingleProduct
